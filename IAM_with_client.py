2c45b3432905eb44994ae0af66a54da0b7be2f54
Atharva <atharvabhise1@gmail.com>
2026-01-28 08:01:38 +0530

    IAM operations using client v2

--- IAM_with_client.py
+++ IAM_with_client.py
@@ -1,4 +1,5 @@
 import os
+import json
 import boto3
 from botocore.exceptions import ClientError
 
@@ -36,9 +37,65 @@ def list_users():
     for data in response['Users']:
         print(data['UserName'])
 
+# create policy
+def create_policy(policyname, policy_path):
+
+    try:
+        with open(policy_path, 'r') as f:
+            policydocument = json.load(f)
+
+        policydocument = json.dumps(policydocument)    
+        print(policydocument)
+
+        iam_client = boto3.client('iam')
+        response = iam_client.create_policy(
+            PolicyName = policyname,
+            PolicyDocument = policydocument
+            
+        )
+        print(response)
+        print('Policy created.')
+
+    except ClientError as e:
+        print(e)
+
+# list policy
+def list_policy():
+    iam_client = boto3.client('iam')
+    response = iam_client.list_policies(
+        Scope = 'Local'
+    )
+    for policy in response['Policies']:
+        print(policy['PolicyName'])
+
+#fetch arn of policy
+def get_arn(policyname):
+    iam_client = boto3.client('iam')
+    response = iam_client.list_policies( Scope = 'Local' )
+    for policy in response['Policies']:
+        if policy['PolicyName'] == policyname:
+            return policy['Arn']
+    return False
+
+# delete policy
+def delete_policy(policyname):
+    policyarn = get_arn(policyname)
+    print(policyarn)
+
+    if policyarn:
+        iam_client = boto3.client('iam')
+        response = iam_client.delete_policy(
+            PolicyArn = policyarn
+        )
+        print(response)
+    else:
+        print("Policy not found")
+
 
 if __name__ == "__main__":
     #create_iam_user("sam")
     #create_iam_user("john")
     list_users()
-    #delete_user("john")
+    delete_user("sam")
+    #create_policy("sam_ec2_full_access", "./data/ec2_access_policy.json")
+    #delete_policy("sam_ec2_full_access")
