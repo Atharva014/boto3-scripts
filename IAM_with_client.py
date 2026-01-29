b8dfeab1fd522563088dc75b919a158aaf80ecac
Atharva <atharvabhise1@gmail.com>
2026-01-29 10:17:52 +0530

    IAM attach/detach policy to user

--- IAM_with_client.py
+++ IAM_with_client.py
@@ -69,9 +69,9 @@ def list_policy():
         print(policy['PolicyName'])
 
 #fetch arn of policy
-def get_arn(policyname):
+def get_arn(policyname, scope):
     iam_client = boto3.client('iam')
-    response = iam_client.list_policies( Scope = 'Local' )
+    response = iam_client.list_policies( Scope = scope )
     for policy in response['Policies']:
         if policy['PolicyName'] == policyname:
             return policy['Arn']
@@ -79,7 +79,7 @@ def get_arn(policyname):
 
 # delete policy
 def delete_policy(policyname):
-    policyarn = get_arn(policyname)
+    policyarn = get_arn(policyname, 'Local')
     print(policyarn)
 
     if policyarn:
@@ -91,11 +91,103 @@ def delete_policy(policyname):
     else:
         print("Policy not found")
 
+# attach custom policy to user
+def attach_cutsom_poilicy_to_user(policyname, username):
+    policyarn = get_arn(policyname, 'Local')
+    print(policyarn)
+
+    if policyarn:
+        try:
+            iam_client = boto3.client('iam')
+            response = iam_client.attach_user_policy(
+                UserName = username,
+                PolicyArn = policyarn
+            )
+            print(response)
+        except ClientError as e:
+            if e.response['Error']['Code'] == 'EntityAlreadyExists':
+                print("Object already exists")
+            else:
+                print("Unexpected error: %s" % e)
+
+    else:
+        print("Policy not found")
+
+# detach custom policy to user
+def detach_cutsom_poilicy_to_user(policyname, username):
+    policyarn = get_arn(policyname, 'Local')
+    print(policyarn)
+
+    if policyarn:
+        try:
+            iam_client = boto3.client('iam')
+            response = iam_client.detach_user_policy(
+                UserName = username,
+                PolicyArn = policyarn
+            )
+            print(response)
+        except ClientError as e:
+            if e.response['Error']['Code'] == 'EntityAlreadyExists':
+                print("Object already exists")
+            else:
+                print("Unexpected error: %s" % e)
+
+    else:
+        print("Policy not found")
+
+# attach managed policy to user
+def attach_managed_poilicy_to_user(policyname, username):
+    policyarn = get_arn(policyname, 'AWS')
+    print(policyarn)
+
+    if policyarn:
+        try:
+            iam_client = boto3.client('iam')
+            response = iam_client.attach_user_policy(
+                UserName = username,
+                PolicyArn = policyarn
+            )
+            print(response)
+        except ClientError as e:
+            if e.response['Error']['Code'] == 'EntityAlreadyExists':
+                print("Object already exists")
+            else:
+                print("Unexpected error: %s" % e)
+
+    else:
+        print("Policy not found")
+
+# detach managed policy to user
+def detach_managed_poilicy_to_user(policyname, username):
+    policyarn = get_arn(policyname, 'AWS')
+    print(policyarn)
+
+    if policyarn:
+        try:
+            iam_client = boto3.client('iam')
+            response = iam_client.detach_user_policy(
+                UserName = username,
+                PolicyArn = policyarn
+            )
+            print(response)
+        except ClientError as e:
+            if e.response['Error']['Code'] == 'EntityAlreadyExists':
+                print("Object already exists")
+            else:
+                print("Unexpected error: %s" % e)
+
+    else:
+        print("Policy not found")
+
 
 if __name__ == "__main__":
     #create_iam_user("sam")
     #create_iam_user("john")
     list_users()
-    delete_user("sam")
+    #delete_user("sam")
     #create_policy("sam_ec2_full_access", "./data/ec2_access_policy.json")
-    #delete_policy("sam_ec2_full_access")
+    delete_policy("sam_ec2_full_access")
+    #attach_cutsom_poilicy_to_user("sam_ec2_full_access", "sam")
+    #detach_cutsom_poilicy_to_user("sam_ec2_full_access", "sam")
+    #attach_managed_poilicy_to_user("AdministratorAccess", "sam")
+    #detach_managed_poilicy_to_user("AdministratorAccess", "sam")
